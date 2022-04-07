a1b21e4dec7731d43e7ca043bfb532f5877db811
wgiese <wolfgang.giese@mdc-berlin.de>
2022-04-07 15:01:55 +0200

    added option to load pretrained cellpose model or use other models e.g. cyto2

--- vascu_ec/vascu_ec/utils/seg.py
+++ vascu_ec/vascu_ec/utils/seg.py
@@ -3,28 +3,39 @@ from pathlib import Path
 import cellpose.models
 import numpy as np
 import skimage.segmentation
+from skimage import morphology
 from scipy import ndimage as ndi
 
 from vascu_ec.vascu_ec_logging import get_logger
 
 
-def get_cellpose_model(use_gpu):
-    """Gets the cellpose default model"""
-    return cellpose.models.Cellpose(gpu=use_gpu, model_type='cyto')
+def get_cellpose_model(parameters):
+    """Gets the specified cellpose model"""
+
+    if parameters["cp_model_type"] == "custom":
+        model = cellpose.models.CellposeModel(gpu=uparameters["use_gpu"], pretrained_model = parameters["cp_model_path"])
+    else:
+        model = cellpose.models.Cellpose(gpu=parameters["use_gpu"], model_type=parameters["cp_model_type"])    
+    
+    return model
 
 
 def get_cellpose_segmentation(parameters, im_seg):
     """Gets the cellpose segmentation"""
     get_logger().info("Calculate cellpose segmentation. This might take some time...")
 
-    model = get_cellpose_model(parameters["use_gpu"])
+    model = get_cellpose_model(parameters)
     if parameters["channel_nucleus"] >= 0:
         channels = [1, 2]
     else:
         channels = [0, 0]
 
     # masks, flows, styles, diams = model.eval(im_seg, channels=channels)
-    masks, flows, styles, diams = model.eval(im_seg, diameter=parameters["estimated_cell_diameter"], channels=channels)
+
+    if parameters["cp_model_type"] == "custom":
+        masks, flows, styles = model.eval(im_seg, diameter=parameters["estimated_cell_diameter"], channels=channels)
+    else:    
+        masks, flows, styles, diams = model.eval(im_seg, diameter=parameters["estimated_cell_diameter"], channels=channels)
 
     return masks
 
@@ -50,7 +61,11 @@ def load_or_get_cellpose_segmentation(parameters, img_seg, filepath):
         cellpose_mask = cellpose_mask_clear_border
 
         get_logger().info("Removed number of cellpose borders: %s" % number_of_cellpose_borders)
+        
+        #TODO: remove small objects here
+        #cellpose_mask_remove_small_objects = morphology.remove_small_objects(masks, parameters["min_macrophage_area"], connectivity=2)
 
+ 
     get_logger().info("Detected number of cellpose labels: %s" % len(np.unique(cellpose_mask)))
 
     return cellpose_mask
