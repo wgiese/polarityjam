83c34b64c55b4d597bdd4cafc1fde871e6a03655
morphogenetics <33558091+morphogenetics@users.noreply.github.com>
2022-05-13 15:27:30 +0200

    Update feature_extraction.py
    first and second neighborhood difs for each node in a rag from a instance segmentation

--- polarityjam/polarityjam/feature_extraction.py
+++ polarityjam/polarityjam/feature_extraction.py
@@ -550,3 +550,72 @@ def morans_I(G,feature):
     weightedCovariance = weightedCov
     moransI = (numberOfValues * weightedCovariance) / (sumOfWeightMatrix * summedVariance)
     return(moransI)
+# Foi = "area" this could be area or perimeter or something else continous
+# Graph = the current region adjacency matrix
+# ordered list of nodes, the order that the nodes have been iterated thru before
+# to populate the image properties area
+# In my mind this would be : np.unique(cell_mask_rem_island) from line 217 
+# could would be run after morans has been done, need a rag with nodes features.
+# Make these compatible with periodic measurements
+
+#function that iterates through nodes in a graph
+#for each node n in all nodes of graph 
+#focal_node n - second nearest neighbors
+#for a list of second nearest nieghbor focal node diffs
+#mean, median, variance and range are calculated for dif
+#list of these diffs are returned
+def second_neighbor_dif(graph, foi, ordered_list_of_nodes):
+    mean_dif_ar =[]
+    median_dif_ar =[]
+    var_dif_ar =[]
+    range_dif_ar = []
+    for n in graph.nodes():
+        focal_foi = (graph.nodes[n][foi])
+        second_nbrs = second_neighbors(graph,n)
+        val_ar = []
+        for once_removed in second_nbrs:
+            second_n = (graph.nodes[once_removed][foi])
+            diff_val = (second_n - focal_foi)
+            val_ar.append(diff_val)
+
+        mean_dif_focal = np.mean(val_ar)
+        median_dif_focal = np.median(val_ar)
+        var_dif_focal = np.var(val_ar)
+        range_focal_dif = abs(np.min(val_ar) - np.max(val_ar))
+
+        mean_dif_ar.append(mean_dif_focal)
+        median_dif_ar.append(median_dif_focal)
+        var_dif_ar.append(var_dif_focal)
+        range_dif_ar.append(range_focal_dif)
+    return(mean_dif_ar,median_dif_ar,var_dif_ar,range_dif_ar)
+
+#function that iterates through nodes in a graph
+#for each node n in all nodes of graph 
+#focal_node n - nearest neighbors
+#for a list of nearest nieghbor focal node diffs
+#mean, median, variance and range are calculated for dif
+#list of these diffs are returned
+def neighbor_dif(graph, foi, ordered_list_of_nodes):
+    mean_dif_ar =[]
+    median_dif_ar =[]
+    var_dif_ar =[]
+    range_dif_ar = []
+    for n in graph.nodes():
+        focal_foi = (graph.nodes[n][foi])
+        second_nbrs = shared_edges(graph,n)
+        val_ar = []
+        for once_removed in second_nbrs:
+            second_n = (graph.nodes[once_removed][foi])
+            diff_val = (second_n - focal_foi)
+            val_ar.append(diff_val)
+
+        mean_dif_focal = np.mean(val_ar)
+        median_dif_focal = np.median(val_ar)
+        var_dif_focal = np.var(val_ar)
+        range_focal_dif = abs(np.min(val_ar) - np.max(val_ar))
+
+        mean_dif_ar.append(mean_dif_focal)
+        median_dif_ar.append(median_dif_focal)
+        var_dif_ar.append(var_dif_focal)
+        range_dif_ar.append(range_focal_dif)
+    return(mean_dif_ar,median_dif_ar,var_dif_ar,range_dif_ar)
